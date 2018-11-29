134f271254685591d26699e2415e259812b456fb
Briandito Priambodo <briandito.p@gmail.com>
2018-11-29 16:49:07 +0800

    Initial Commit

---
+++ LearnCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnCoreData
+//
+//  Created by Briandito Priambodo on 29/11/18.
+//  Copyright © 2018 Briandito Priambodo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
